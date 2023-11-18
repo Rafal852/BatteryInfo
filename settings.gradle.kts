63a24c074fcc96e66ad4df9a5b9c45fcbfe4def0
Rafałjgfhjgfjf <rafal,oerfikowski@onet.pl>
2023-11-18 18:56:27 +0100

    BatteryInfo

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://dl.bintray.com/ssynhtn-org/android-wave-view") }
+    }
+}
+
+rootProject.name = "BatteryInfo"
+include(":app")
+ 
